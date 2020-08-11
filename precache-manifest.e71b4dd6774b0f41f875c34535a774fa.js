c8bc0ac725e70c384cd0f3402a2dc41af411d4a4
sasha.kalinskiy@yandex.ru <jsn364cmd025>
2020-08-12 01:23:38 +0300

    Updates

---
+++ precache-manifest.e71b4dd6774b0f41f875c34535a774fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16df9f6dc03e0b883ad3adbcaadc5799",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "4e7a503b9f53122349aa",
+    "url": "/portfolio_react/static/css/main.052d73da.chunk.css"
+  },
+  {
+    "revision": "c3d32b0dfd5af8dd10ea",
+    "url": "/portfolio_react/static/js/2.8f228705.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_react/static/js/2.8f228705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7a503b9f53122349aa",
+    "url": "/portfolio_react/static/js/main.2a017a31.chunk.js"
+  },
+  {
+    "revision": "8ad38dfdeabc498f2de6",
+    "url": "/portfolio_react/static/js/runtime-main.3e6fd58f.js"
+  },
+  {
+    "revision": "fbd87e1bb1a75c97912e2603d201c9c7",
+    "url": "/portfolio_react/static/media/hero-bg.fbd87e1b.png"
+  },
+  {
+    "revision": "f636c6b7dbf5ef324a35971e13d7776a",
+    "url": "/portfolio_react/static/media/img-1.f636c6b7.png"
+  },
+  {
+    "revision": "c641b8d6b7eb9c8c35c753574d5edc9d",
+    "url": "/portfolio_react/static/media/img-2.c641b8d6.png"
+  }
+]);
